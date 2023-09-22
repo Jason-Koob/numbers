e0d220b59111b2bc8e2751b6913e1b662f6df5c8
Jason Spears <64575009+Jason-Koob@users.noreply.github.com>
2023-09-21 21:04:33 -0400

    Evens and Odds

--- numbers.py
+++ numbers.py
@@ -8,11 +8,27 @@ class num:
 print("How many values would you like to check?: ")
 val = int(input())
 
-while len(num.begin) <= val:
+# GET ALL NUMS
+while len(num.begin) < val:
     num.begin.append(len(num.begin))
-    
-print("Numbers Checked: " + str(num.begin))
 
-# for num in num.begin:
-#     if num % 2 == 0:
-#         num.even.append(str(num))
+    # EVENS
+    if len(num.begin)% 2 == 0:
+        num.even.append(len(num.begin))
+
+    # ODDS
+    else:
+        num.odd.append(len(num.begin))
+
+    # PRIME
+    if len(num.begin):
+        num.prime.append(len(num.begin))
+
+    # COMPOSITE
+    else:
+        num.composite.append(len(num.begin))
+    
+print("Even Numbers:          " + str(num.even))
+print("Odd Numbers:           " + str(num.odd))
+print("Prime Numbers:         " + str(num.prime))
+print("Composite Numbers:     " + str(num.composite))
